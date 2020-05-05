088f0c4e704d37dbc1a9bda1f606a7640fa9a98f
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2020-05-05 07:24:12 +0100

    renamed mocked_method.go to mocked_func.go

--- mocked_func.go
+++ mocked_func.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 )
 
-type MockedMethod struct {
+type MockedFunc struct {
 	name  string
 	calls []MethodCall
 }
 
-func (v *MockedMethod) Name() string {
+func (v *MockedFunc) Name() string {
 	return v.name
 }
 
-func (v *MockedMethod) Called(args ...Argument) {
+func (v *MockedFunc) Called(args ...Argument) {
 	var methodCall = MethodCall{args: make(map[string]interface{}, len(args))}
 	for _, arg := range args {
 		methodCall.args[arg.name] = arg.Value
@@ -21,24 +21,24 @@ func (v *MockedMethod) Called(args ...Argument) {
 	v.calls = append(v.calls, methodCall)
 }
 
-func (v *MockedMethod) Calls() []MethodCall {
+func (v *MockedFunc) Calls() []MethodCall {
 	calls := make([]MethodCall, len(v.calls))
 	copy(calls, v.calls)
 	return calls
 }
 
-func NewMockedMethod(name string) *MockedMethod {
-	return &MockedMethod{name: name}
+func NewMockedMethod(name string) *MockedFunc {
+	return &MockedFunc{name: name}
 }
 
-func (v *MockedMethod) AssertCalledExactly(expected int) string {
+func (v *MockedFunc) AssertCalledExactly(expected int) string {
 	if n := len(v.calls); n != expected {
 		return fmt.Sprintf("expected to get %v calls to %v(), got: %v", expected, v.name, n)
 	}
 	return ""
 }
 
-func (v *MockedMethod) AssertCalledAtLeastOnce() string {
+func (v *MockedFunc) AssertCalledAtLeastOnce() string {
 	if n := len(v.calls); n == 0 {
 		return fmt.Sprintf("expected to get at least 1 call to %v(), got none", v.name)
 	}
